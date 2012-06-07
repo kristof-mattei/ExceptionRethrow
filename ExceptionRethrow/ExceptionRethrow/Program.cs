04538858153aed16c2ca85dab3b45a03037f6c4c
Kristof Mattei <kristof@kristofmattei.be>
2012-06-07 15:01:44 +0200

    Initial import

---
+++ ExceptionRethrow/ExceptionRethrow/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConsoleApplication20
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			try
+			{
+				Foo();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message + Environment.NewLine + e.StackTrace);
+				Console.Read();
+			}
+		}
+
+		private static void Foo()
+		{
+			try
+			{
+				Bar();
+			}
+			catch (Exception e)
+			{
+				// handle exception
+				// ...
+				// ...
+
+				throw e;
+			}
+		}
+
+		private static void Bar()
+		{
+			throw new Exception("There is a glitch in the matrix");
+		}
+	}
+}
